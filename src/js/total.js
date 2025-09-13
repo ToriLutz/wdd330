21224cf7aa2f868dc6a39d89f87cbb466ff1ff12
ToriLutz <vglutz@comcast.net>
2025-09-13 15:15:03 -0700

    hidden in cart

--- src/js/total.js
+++ src/js/total.js
@@ -12,7 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const price = parseFloat(item.FinalPrice);
     const quantity = item.Quantity || 1; 
     totalAmount += price * quantity;
+
   });
+if (totalAmount === 0){
+        cTotal.classList.add ('hide');
+    }
+else {
+        cTotal.classList.remove('hide');
+};
 
   cTotal.innerHTML = `<p class="amount">Total Amount: $${totalAmount.toFixed(2)}</p>`;
 });
