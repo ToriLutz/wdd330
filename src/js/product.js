3a13bf7fcb9c023f4f05575afb4d57e78ad47504
ToriLutz <vglutz@comcast.net>
2025-09-13 15:21:24 -0700

    removed unneccassary

--- src/js/product.js
+++ src/js/product.js
@@ -10,23 +10,3 @@ const product = new ProductDetails(productId, dataSource);
 product.init();
 
 console.log(dataSource.findProductById(productId));
-
-function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
